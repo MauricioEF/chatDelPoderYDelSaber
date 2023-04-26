e1f9a5bdcbd33edaff7a3b82099303b45e7407e7
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2023-04-26 08:45:15 -0600

    Server config for deployment

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ import viewsRouter from './routes/views.router.js';
 import __dirname from './utils.js';
 
 const app = express();
-const server = app.listen(8080,()=>console.log(`Listening on 8080`));
+const PORT = process.env.PORT||8080;
+const server = app.listen(PORT,()=>console.log(`Listening on 8080`));
 
 app.engine('handlebars',handlebars.engine());
 app.set('views',`${__dirname}/views`);
